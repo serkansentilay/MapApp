d001c16b3afed022b3c948c30a72385809e75db4
serkansentilay <sentiilay@gmail.com>
2023-07-26 18:37:25 +0300

    MapApp
    Gelecegiyazanlar react native 201 2. Map App

--- App.js
+++ App.js
@@ -1,11 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import MapView from 'react-native-maps';
+import { StyleSheet, View } from 'react-native';
+import * as Location from 'expo-location';
 
 export default function App() {
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location.coords);
+
+    })();
+
+
+    setTimeout(() => {
+      setLocation({
+        latitude: 36.8428171,
+        longitude: 30.8716717,
+      })
+    }, 3000);
+
+  }, []);
+
+  if (errorMsg) {
+    return <Text>{errorMsg}</Text>
+  }
+  console.log("location", location)
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {location &&
+        <MapView style={styles.map}
+          initialRegion={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+          region={{
+            latitude: location?.latitude,
+            longitude: location?.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+
+        />
+      }
+
     </View>
   );
 }
@@ -13,8 +64,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  map: {
+    width: '100%',
+    height: '100%',
   },
 });
